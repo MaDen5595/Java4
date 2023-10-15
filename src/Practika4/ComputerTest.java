4a4d7fa7b9b17af9349efd017a9cbe9895507f21
MaDen5595 <75423860+MaDen5595@users.noreply.github.com>
2023-10-15 20:04:18 +0300

    Update ComputerTest.java
    New ComputerTest

--- src/Practika4/ComputerTest.java
+++ src/Practika4/ComputerTest.java
@@ -1,17 +1,24 @@
 package Practika4;
-
+import java.util.Objects;
+import java.util.Scanner;
 public class ComputerTest {
     public static void main(String[] args) {
-        Computer mine = Computer.ASUS;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Выберите компьютер из следующих(напишите его название) - HP, Asus, MSI, Lenovo, Acer");
+        String c = scan.nextLine();
+        Computer mine = Computer.HP;
+        switch(c){
+            case("HP") -> mine = Computer.HP;
+            case("Asus") -> mine = Computer.ASUS;
+            case("MSI") -> mine = Computer.MSI;
+            case("Lenovo") -> mine = Computer.LENOVO;
+            case("Acer") -> mine = Computer.ACER;
+        }
+        System.out.println("Хороший выбор, вот память вашего компьютера");
         System.out.println(mine.getMemory().toString());
+        System.out.println("Память хорошая, но мы её заменим на следующую:");
         ComputerMemory mem = new ComputerMemory("Custom", "Custom", 12,3.5,true);
         mine.setMemory(mem);
         System.out.println(mine.getMemory().toString());
-        for(Computer c: Computer.values()){
-            System.out.println(c);
-            System.out.println(c.getProcessor().toString());
-            System.out.println(c.getMemory().toString());
-            System.out.println(c.getMonitor().toString());
-        }
     }
 }
